70654c7689b6ead5fbf48f11aa84dc7739151b73
cmmerritt <christiane.merritt@gmail.com>
2021-07-28 12:14:13 -0700

    attempt to make match work

--- src/components/queens/QueenDetail.jsx
+++ src/components/queens/QueenDetail.jsx
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import { fetchQueenById } from '../../services/dragRaceApi';
-import { useParams } from 'react-router-dom';
+
 
 export default class QueenDetail extends Component {
   state = {
-    queen: []
+    queen: [],
+    loading: true
   };
 
   async componentDidMount() {
     // const queenToFetch  = this.props;
 
-    const { id } = useParams();
+    const { match } = this.props;
 
     try {
-      const queen = await fetchQueenById(id);
+      const queen = await fetchQueenById(match.params.id);
       // const queen = await fetchQueenById(queenToFetch.match.params.id);
       this.setState({ queen });
     } catch(err) {
       console.log(err);
+    } finally {
+      this.setState({ loading: false });
     }
   }
 
@@ -27,7 +30,7 @@ export default class QueenDetail extends Component {
     return (
       <>
         <h1>{queen.name}</h1>
-        <img src={queen.image}/>
+        <img src={queen.image} alt={queen.name}/>
         <p>Words of Wisdom: {queen.quote}</p>
       </>
     );
